741ca53c5b4283a9b9b44a7007fb9703743843e1
Renan Medeiros <renanmedeiros79@gmail.com>
2024-08-07 18:19:28 -0300

    Criando um arquivo styles/theme.ts para configurar o tema do Material-UI

---
+++ styles/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1DB954',
+    },
+    background: {
+      default: '#121212',
+      paper: '#181818',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+  },
+});
+
+export default theme;
