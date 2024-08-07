5f7e1c254ad6255401d01fce1f7026773a13c2c2
Renan Medeiros <renanmedeiros79@gmail.com>
2024-08-07 18:21:13 -0300

    Configurando o _app.tsx para Usar Material-UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../styles/theme';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
